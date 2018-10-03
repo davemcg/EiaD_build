5200f70db5359445444447ea71a31513c29b9e6b
Vinay Swamy <swamyvinny@gmail.com>
2018-10-03 11:04:06 -0400

    fixed for rmats body tissues

--- scripts/preprMATS.R
+++ scripts/preprMATS.R
@@ -8,10 +8,19 @@ sample_design <-data.frame(sample_design, path=sapply(sample_design$sample_acces
 
 #ignore single ended files for now
 sample_design <- sample_design[!is.na(sample_design$path),]
-for(type in unique(sample_design$sub.tissue)){
+ttypes=unique(sample_design$sub.tissue)[-7]
+for(type in ttypes){
   df <- filter(sample_design, sub.tissue==type ,paired=='y')
   writeLines(df$path,paste0('ref/',gsub(' ', '.' ,type),'_PE','.rmats.txt'),sep = ',')
   df <- filter(sample_design, sub.tissue==type ,paired=='n')
   writeLines(df$path,paste0('ref/',gsub(' ', '.' ,type),'_SE','.rmats.txt'),sep = ',')
 }
+
+body=read.table('synth_body.txt', stringsAsFactors=F,header=F)
+for(i in 1:nrow(body)){
+	paste0('STARbams_realigned/',body[1,i],'_PE' ,'/Aligned.out.bam')%>%writeLines('ref/body_PE.rmats.txt',sep=',')
+	paste0('STARbams_realigned/',body[1,i],'_SE' ,'/Aligned.out.bam')%>%writeLines('ref/body_SE.rmats.txt',sep=',')
+}
+
+
 
