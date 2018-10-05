6229d7781dcace3276f4dcaa6f76ca827d2138a5
Vinay Swamy <vinayswamy@Vinays-MacBook-Pro.local>
2018-10-05 10:37:25 -0400

    fixed to use for PA

--- scripts/filterFasta.py
+++ scripts/filterFasta.py
@@ -1,8 +1,10 @@
-with open('ref/gencodeRef.fa') as infasta, open('tx_for_removal') as bad_tx, open('ref/gencodeRef_trimmed.fa','w+') as outfasta:
+import sys
+
+with open(sys.argv[1]) as infasta, open(sys.argv[2]) as bad_tx, open(sys.argv[3],'w+') as outfasta:
     names=set()
     for line in bad_tx:
-        names.add('>'+line.strip())
-    oldline=infasta.readline().strip().split('|')[0]
+        names.add(line.strip())
+    oldline=infasta.readline().strip()
     while oldline:
         if oldline not in names and '>' in oldline:
             write=True
@@ -10,4 +12,4 @@ with open('ref/gencodeRef.fa') as infasta, open('tx_for_removal') as bad_tx, ope
             write=False
         if write:
             outfasta.write(oldline+'\n')
-        oldline=infasta.readline().strip().strip().split('|')[0]
+        oldline=infasta.readline().strip()
