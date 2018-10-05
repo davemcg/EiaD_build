7355c2b7b059088e37ce26dc78e8e33da6c401c3
Vinay Swamy <swamyvinny@gmail.com>
2018-10-05 16:41:50 -0400

    shell commands

--- scripts/clean_gtf.R
+++ scripts/clean_gtf.R
@@ -1,29 +1,36 @@
 setwd('/data/swamyvs/autoRNAseq')
 library(rtracklayer)
 library(dplyr)
+library(biomaRt)
 gtf <- readGFF('ref/comb.gtf')
-ref <- readGFF('ref/gencodeAno_bsc.gtf')
-gtf <- gtf[!is.na(gtf$ref_gene_id),]
-gtf[is.na(gtf)] <- '.'
-ref_genes <- filter(ref,type=='gene')
+###this script is bad and should feel bad
+#remove single exon transcripts
+tx_counts <- table(gtf$transcript_id)
+keep <- names(tx_counts[tx_counts>2])
+gtf <-gtf[gtf$transcript_id%in%keep,]
+gtf$seqid <- as.character(gtf$seqid)
+gtf$type <- as.character(gtf$type)
+#gtf <-gtf[grep('chr',gtf$seqid),]
+#fix some formatting
+ref_gtf <- readGFF('ref/gencodeAno_bsc.gtf')%>%filter(gene_type=="protein_coding" )
+ref_gtf$seqid <- as.character(ref_gtf$seqid)
+gtf.tmp <- gtf
+ref_gtf$seqid[ref_gtf$seqid=='chrX'] <- gtf$seqid[gtf$seqid=='chrX'] <- 'chr23'
+ref_gtf$seqid[ref_gtf$seqid=='chrY'] <- gtf$seqid[gtf$seqid=='chrY'] <- 'chr24'
+ref_gtf$seqid[ref_gtf$seqid=='chrM'] <- gtf$seqid[gtf$seqid=='chrM'] <- 'chr25'
+ref_bed <- data.frame(seqid=strsplit(ref_gtf$seqid,'r')%>%sapply(function(x)x[2]),ref_gtf$start,ref_gtf$end,ref_gtf$gene_name,
+                      scor='.',strand=ref_gtf$strand, stringsAsFactors = F)
+write.table(ref_bed,'refgtf.bed',col.names = F,row.names = F,quote = F,sep = '\t',na='.')
+gtf.missing_genename <- gtf[is.na(gtf$ref_gene_id),]%>%filter(type=='transcript')
+bed <- data.frame(seqid=strsplit(gtf.missing_genename$seqid,'r')%>%sapply(function(x)x[2]),gtf.missing_genename$start,
+                  gtf.missing_genename$end,gtf.missing_genename$transcript_id,score='.',strand=gtf.missing_genename$strand,
+                  stringsAsFactors = F)
+write.table(bed,'missing.bed',col.names = F,row.names = F,quote = F,sep ='\t',na='.')
+colnames(bed) <- c('V1','V2','V3','transcript_id')
+bed$V1 <- as.integer(bed$V1)
+#read in bedtools result
+save.image("~/NIH/autoRNAseq/tmp.RData")
+#system2('bedtools intersect -f .5  -wo -s -a missing.bed -b refgtf.bed > testout.bed')
 
-ref_genes <- ref_genes[ref_genes$gene_id%in%gtf$ref_gene_id,  ]
-
-
-gtf <- gtf[,c(1:8,13,10,12,11)]
-ref_genes <- ref_genes[,c(1:9,14,11,20)]
-cn <- c("seqid", "source", "type", "start", "end" ,"score", "strand", "phase" ,"gene_id", "transcript_id" ,"gene_name", "exon_number"    )
-colnames(gtf) <- colnames(ref_genes) <- cn
-
-gtf_final <- rbind(gtf,ref_genes)
-gtf_final[is.na(gtf_final)] <- '.'
-to_write <- paste( paste0('gene_id ',gtf_final$gene_id),
-                       paste0(' transcript_id ','"',gtf_final$transcript_id,'"'),
-                       paste0(' exon_numer ','"',gtf_final$exon_number,'"'),
-                       paste0(' gene_name ','"',gtf_final$gene_name,'"'),sep = ';')
-gtf_final$to_write <- to_write  
-gtf_final <- arrange(gtf_final, seqid, start, desc(type))
-gtf_final <- arrange(gtf_final, seqid,start, gene_id, transcript_id,type)
-write.table(gtf_final[,c(1:8,13)],'ref/combined_final.gtf',sep = '\t',quote = F, col.names = F,row.names = F)
 
 
