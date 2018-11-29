a16a3d1f03c120690c53c69bab7e963d7d0a9fb0
Vinay Swamy <swamyvinny@gmail.com>
2018-11-29 13:41:05 -0500

    added new samples

--- scripts/make_sample_table_for_new_samples.R
+++ scripts/make_sample_table_for_new_samples.R
@@ -5,9 +5,9 @@ library(tidyverse)
 library(stringr)
 library(dplyr)
 #getSRAdbFile(destdir='ref/',destfile='SRAmetadb.11-06-18.sqlite.gz') # do periodically. latest from 2017-01-19, 2017-04-17
-sqlfile <- 'ref/SRAmetadb.11-06-18.sqlite' 
+sqlfile <- '~/NIH/autoRNAseq/ref/SRAmetadb.11-06-18.sqlite' 
 sra_con <- dbConnect(RSQLite::SQLite(),sqlfile)
-new_samples <- read.csv('new_studies_08_28.csv',stringsAsFactors = F,comment.char = '#')
+new_samples <- read.table('samples_geo',stringsAsFactors = F,sep='\n')[,1]%>%as.character()
 res <- list()
 for(i in 1:nrow(new_samples)){
     targ <- new_samples[i,]
@@ -21,9 +21,9 @@ tab <- tab[!is.na(tab$sample),]
 tab$sample <- gsub(' ','',tab$sample)
 tab <- filter(tab, !sample%in%c('SRS1747723','SRS1588794','SRS1588796','SRS1747748','SRS1747747','SRS1747749'))# aleady in sample
 #tab <- filter(tab, !grepl('SRX',sample)) # not in db
-que <- "SELECT run_accession,library_layout FROM sra WHERE sample_accession='BLANK'"
-info <- lapply(tab$sample,function(x) dbGetQuery(sra_con, gsub('BLANK',x,que)))
-to_dl <- do.call(rbind, info)[,1]
+que <- "SELECT study_accession FROM sra WHERE sample_accession='BLANK'"
+info <- lapply(tab$V1,function(x) dbGetQuery(sra_con, gsub('BLANK',x,que)))
+to_dl <- do.call(rbind, info)
 write(to_dl, 'new_runs_tdl',sep = 'n')
 info_runs <- lapply(info,function(x) paste(x[,1], collapse = ','))
 info_paired <- lapply(info, function(x) ifelse(any(grepl('PAIRED',x[,2])),'y','n'))
@@ -34,14 +34,19 @@ old_tab <- read.table('sampleTable1015_tissues.tab',stringsAsFactors = F, header
 old_tab$V6 <- 'old'
 colnames(old_tab) <- colnames(tab)
 full_tab <- rbind(old_tab,tab)
+full_tab <- full_tab[!duplicated(full_tab[,1]),]
 write.table(full_tab,'sampleTable1106_tissues.tab',row.names = F,col.names = F,sep = '\t',quote = F)
 b <- !full_tab$tissue%in%c('Retina','Cornea','RPE','ESC')
 full_tab$tissue[b] <- 'body'
 full_tab$subtissue[b] <- 'body'
 write.table(full_tab,'sampleTable1106.tab',row.names = F,col.names = F,sep = '\t',quote = F)
+#######
 
-
-
-
-
-
+new <- read.csv('samples_geo.csv', header = F, stringsAsFactors = F)
+old <- read.table('sampleTable1106_tissues.tab', stringsAsFactors = F)
+comb <- rbind(old,new, stringsAsFactors=F)
+write.table(comb, 'sampleTable1127_tissues.tab', sep = '\t', row.names = F, col.names = F, quote = F)
+b <- !comb$V4%in%c('Retina','Cornea','RPE','ESC')
+comb$V4[b] <- 'body'
+comb$V5[b] <- 'body'
+write.table(comb, 'sampleTable1127.tab', sep = '\t', row.names = F, col.names = F, quote = F)
