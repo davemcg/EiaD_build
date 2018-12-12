dcd626098e96c58dfa75032217dd53c1e5e55ffb
David McGaughey <mcgaughey@gmail.com>
2018-12-12 15:51:43 -0500

    add e-mtab-4377 data manually as not in SRA

--- scripts/make_meta_info.R
+++ scripts/make_meta_info.R
@@ -18,10 +18,19 @@ sra_con <- dbConnect(RSQLite::SQLite(),sql_file)
 sra_query <- "SELECT * FROM sra WHERE sample_accession='BLANK'"
 eye_tissue <- c('Retina','RPE','Cornea','ESC')
 core_tight <- lapply(sample_design$sample_accession,function(x) dbGetQuery(sra_con, gsub('BLANK',x,sra_query)))%>%do.call(rbind,.) %>%
-  distinct%>%select(study_accession ,study_title,study_abstract,sample_accession,sample_attribute) %>%
+  distinct%>%select(study_accession, study_title, study_abstract, sample_accession, run_accession, sample_attribute) %>%
   left_join(sample_design[,c(1,4,5)],by='sample_accession')
 core_tight$Origin <-  strsplit(core_tight[,'Sub_Tissue'],'_') %>% 
   sapply(function(x) ifelse(x[[1]] %in% eye_tissue, x[[2]], 'Tissue'))
+# add EMTAB
+core_tight <- bind_rows(core_tight, 
+                        sample_design %>% 
+                          filter(grepl('MTAB', sample_accession)) %>% 
+                          select(sample_accession, run_accession, Tissue, Sub_Tissue) %>% 
+                          mutate(Origin = 'Adult Tissue',
+                                 study_title = 'RNAseq 50 Normal Human Retina',
+                                 study_accession = 'E-MTAB-4377',
+                                 study_abstract = 'RNA-seq of post-mort retina donor without clinically relevant visual impairment. Ploy-A enriched. 75-nt paired-end. Short time lapse between tissue sampling and cDNA generation.'))
 core_tight <- core_tight %>% 
   as.tibble() %>% 
   rowwise() %>% 
@@ -31,6 +40,7 @@ core_tight <- core_tight %>%
   mutate(Tissue = gsub('_', ' - ', Tissue),
          Sub_Tissue = gsub('_', ' - ', Sub_Tissue),
          Origin = gsub('_', ' - ', Origin))
+
 
 gene_qc_tpms=read_csv(gene_qc_file)
 samples_kept=colnames(gene_qc_tpms) %>% 
