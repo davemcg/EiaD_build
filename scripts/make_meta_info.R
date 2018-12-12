6fb1fd8df1dab22e5ca75998ab455812d1a0224d
David McGaughey <mcgaughey@gmail.com>
2018-12-12 15:33:41 -0500

    clean up metadata formatting and code is cleaner to read

--- scripts/make_meta_info.R
+++ scripts/make_meta_info.R
@@ -1,10 +1,10 @@
 library(tidyverse)
 library(RSQLite)
-library(SRAdb)
+
 args=commandArgs(trailingOnly = T)
 sample_metadata = args[1]
 gtf_file = args[2]
-sqlfile=args[3]
+sql_file=args[3]
 gene_qc_file=args[4]
 tx_qc_file=args[5]
 setwd(args[6])
@@ -13,26 +13,41 @@ tx_file <- args[8]
 gene_file <- args[9]
 
 sample_design <- read.table(sample_metadata, stringsAsFactors = F, header=F, sep = '\t')
-colnames(sample_design) <- c('sample_accession', 'run_accession', 'paired','tissue','sub-tissue','origin')
-sra_con <- dbConnect(RSQLite::SQLite(),sqlfile)
-que <- "SELECT * FROM sra WHERE sample_accession='BLANK'"
+colnames(sample_design) <- c('sample_accession', 'run_accession', 'paired','Tissue','Sub_Tissue','Origin')
+sra_con <- dbConnect(RSQLite::SQLite(),sql_file)
+sra_query <- "SELECT * FROM sra WHERE sample_accession='BLANK'"
 eye_tissue <- c('Retina','RPE','Cornea','ESC')
-core_tight <- lapply(sample_design$sample_accession,function(x) dbGetQuery(sra_con, gsub('BLANK',x,que)))%>%do.call(rbind,.)%>%
-  distinct%>%select(study_accession ,study_title,study_abstract,sample_accession,sample_attribute)%>%
+core_tight <- lapply(sample_design$sample_accession,function(x) dbGetQuery(sra_con, gsub('BLANK',x,sra_query)))%>%do.call(rbind,.) %>%
+  distinct%>%select(study_accession ,study_title,study_abstract,sample_accession,sample_attribute) %>%
   left_join(sample_design[,c(1,4,5)],by='sample_accession')
-core_tight$origin <-  strsplit(core_tight[,'sub-tissue'],'_')%>%sapply(function(x)ifelse(x[[1]]%in%eye_tissue,x[[2]],'Tissue'))
+core_tight$Origin <-  strsplit(core_tight[,'Sub_Tissue'],'_') %>% 
+  sapply(function(x) ifelse(x[[1]] %in% eye_tissue, x[[2]], 'Tissue'))
+core_tight <- core_tight %>% 
+  as.tibble() %>% 
+  rowwise() %>% 
+  mutate(Tissue = gsub('\\.', ' ', Tissue),
+         Sub_Tissue = gsub('\\.', ' ', Sub_Tissue),
+         Origin = gsub('\\.', ' ', Origin)) %>% 
+  mutate(Tissue = gsub('_', ' - ', Tissue),
+         Sub_Tissue = gsub('_', ' - ', Sub_Tissue),
+         Origin = gsub('_', ' - ', Origin))
+
 gene_qc_tpms=read_csv(gene_qc_file)
-samples_kept=colnames(gene_qc_tpms)%>%data.frame(sample_accession=.,kept='kept',stringsAsFactors = F)
+samples_kept=colnames(gene_qc_tpms) %>% 
+  data.frame(sample_accession=.,Kept='Kept',stringsAsFactors = F)
 core_tight=left_join(core_tight,samples_kept, by='sample_accession')
-core_tight$kept[is.na(core_tight$kept)]='removed'
+core_tight$Kept[is.na(core_tight$Kept)]='removed'
 save(core_tight,file = metadata_file)
 
-gtf <- rtracklayer::readGFF(gtf_file) %>% dplyr::filter(type=='transcript')
-anno <- gtf[,c("gene_id", "gene_name", "transcript_id")]%>%filter(gene_name%in%gene_qc_tpms$ID)
+gtf <- rtracklayer::readGFF(gtf_file) %>% 
+  dplyr::filter(type=='transcript')
+anno <- gtf[,c("gene_id", "gene_name", "transcript_id")] %>% 
+  filter(gene_name%in%gene_qc_tpms$ID)
 
 genes= anno$gene_name %>% unique
 save(genes, file= gene_file)
 tx_qc_tpms=read_csv(tx_qc_file)
-tx=gtf[,c("gene_name", "transcript_id")]%>%distinct%>%filter(transcript_id%in%tx_qc_tpms$ID)%>% split(.[,2])%>%
+tx=gtf[,c("gene_name", "transcript_id")] %>%
+  distinct%>%filter(transcript_id% in% tx_qc_tpms$ID)%>% split(.[,2]) %>%
   sapply(function(x) paste0(x[,1],' (',x[,2],')'))
-save(tx, file= tx_file)
+save(tx, file=tx_file)
