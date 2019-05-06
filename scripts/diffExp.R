1a10e1d39a2be8ac898c416b9039a9f87f7d5cf8
David McGaughey <mcgaughey@gmail.com>
2019-05-06 10:56:19 -0400

    fix column names

--- scripts/diffExp.R
+++ scripts/diffExp.R
@@ -5,31 +5,33 @@ library(limma)
 library(edgeR)
 library(qsmooth)
 library(readr)
+library(tidyverse)
 args=commandArgs(trailingOnly = T)
 
 setwd(args[1])
-sample_table <- read.table(args[2],stringsAsFactors = F, header = F,sep = '\t')
+sample_table <- read_tsv(args[2])
+colnames(sample_table)[1] <- 'sample_accession'
 lstpms_smoothed <- read.csv(args[3], row.names = 1)  #'results/smoothed_filtered_tpms.csv',row.names = 1)
 de_comparison_contrast_names_file <- args[4]
 output_limma_object_file <- args[5]
 output_list_of_df_file <- args[6]
 
-colnames(sample_table) <- c('sample','run','paired','tissue','subtissue','origin')
-sample_table <- filter(sample_table, sample %in% colnames(lstpms_smoothed))
-eye_samples <- filter(sample_table,tissue%in%c('Retina','RPE','Cornea','ESC','Lens'))
-body_samples <- filter(sample_table,!tissue%in%c('Retina','RPE','Cornea','ESC', 'Lens', 'RetinalEpithelium'))
+#colnames(sample_table) <- c('sample_acession','run','paired','tissue','subtissue','origin')
+sample_table <- filter(sample_table, sample_accession %in% colnames(lstpms_smoothed))
+eye_samples <- filter(sample_table, Tissue %in% c('Retina','RPE','Cornea','ESC','Lens'))
+body_samples <- filter(sample_table, !Tissue %in% c('Retina','RPE','Cornea','ESC', 'Lens', 'RetinalEpithelium'))
 set.seed(123421)
 gtex_sample <- sample_n(body_samples,176)
 # getting different samples than david, going to use his list
 # DM: nah, let's use yours
 # load('ref/david_gtex_subsamples.RData')# l> gtex_sub_samples
 # gtex_sample <- filter(sample_table,sample%in%gtex_sub_samples)
-gtex_sample$tissue <- gtex_sample$subtissue <-  'Body'
-deg_sample_table <- rbind(eye_samples,gtex_sample,body_samples %>% mutate(subtissue = tissue))
-deg_counts <- lstpms_smoothed[,deg_sample_table$sample]
-subtissue <- deg_sample_table$subtissue%>%as.factor()
-design_eye_and_gtex <- model.matrix(~0 + subtissue )
-colnames(design_eye_and_gtex) <- levels(subtissue)
+gtex_sample$Tissue <- gtex_sample$Sub_Tissue <-  'Body'
+deg_sample_table <- rbind(eye_samples,gtex_sample,body_samples %>% mutate(Sub_Tissue = Tissue))
+deg_counts <- lstpms_smoothed[,deg_sample_table$sample_accession]
+Sub_Tissue <- deg_sample_table$Sub_Tissue %>% as.factor()
+design_eye_and_gtex <- model.matrix(~0 + Sub_Tissue )
+colnames(design_eye_and_gtex) <- levels(Sub_Tissue)
 
 y_eye_gtex <- DGEList(deg_counts)
 y_eye_gtex <- calcNormFactors(y_eye_gtex)
@@ -38,15 +40,15 @@ vfit_eye_gtex <- lmFit(v_eye_gtex, design_eye_and_gtex)
 
 
 
-conts <- combn(unique(subtissue),2) %>%
+conts <- combn(unique(Sub_Tissue),2) %>%
   t() %>%
   data.table() %>%
   mutate(name=paste(V1,V2,sep='_vs_'),
          contrast=paste(V1,V2,sep='-'),
          tissue_2=strsplit(V2,'_|-')%>%lapply(function(x)x[[1]]),
          tissue_1=strsplit(V1,'_|-')%>%lapply(function(x)x[[1]]),
-         type_2= strsplit(V2,'_|-')%>%lapply(function(x) ifelse(x[[1]] %in% grep("_|-",subtissue, value=T, invert = T) %>% unique(),'Adult',x[[2]])),
-         type_1= strsplit(V1,'_|-')%>%lapply(function(x) ifelse(x[[1]] %in% grep("_|-",subtissue, value=T, invert = T) %>% unique(),'Adult',x[[2]])),
+         type_2= strsplit(V2,'_|-')%>%lapply(function(x) ifelse(x[[1]] %in% grep("_|-",Sub_Tissue, value=T, invert = T) %>% unique(),'Adult',x[[2]])),
+         type_1= strsplit(V1,'_|-')%>%lapply(function(x) ifelse(x[[1]] %in% grep("_|-",Sub_Tissue, value=T, invert = T) %>% unique(),'Adult',x[[2]])),
          cont_name= paste0(tissue_1,' (',type_1,') vs ',tissue_2,' (',type_2,')')) %>%
   filter(grepl('RPE|Cornea|Retin|Lens|ESC|RetinalEpithelium', ignore.case=T, contrast))  # remove all notEye-notEye comparisons
 
