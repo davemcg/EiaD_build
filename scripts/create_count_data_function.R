0f8f8859e04ebe6a7e532dbde9a8f191154f5e54
David McGaughey <mcgaughey@gmail.com>
2022-11-07 09:41:39 -0500

    optimize, simplify, and add raw counts

--- scripts/create_count_data_function.R
+++ scripts/create_count_data_function.R
@@ -3,11 +3,14 @@
 create_count_data_frames <- 
   function(recount3_library_url, 
            projects_vector, 
-           count_file_name, 
-           aggregated_count_file_name, 
+           TPM_matrix_file, 
+           long_TPM_matrix_file, 
+           count_matrix_file,
+           long_count_matrix_file,
            mapping_file_name, 
            metadata,
-           empty_cache = TRUE) {
+           empty_cache = TRUE,
+           GTEX = FALSE) {
     
     if (empty_cache){
       print("Clearing recount3 cache")
@@ -16,6 +19,11 @@ create_count_data_frames <-
     #Indicating which recount library the function will pull from
     options(recount3_url = recount3_library_url)
     human_projects <- available_projects()
+    if (GTEX){
+      gtex_data <- subset(human_projects, file_source == "gtex" & project_type == "data_sources")
+    }
+    #Create an empty list to gather the TPM data
+    TPM_data_list <- list()
     #Create an empty list to gather the count data
     counts_data_list <- list()
     #Create an empty list to gather mapping information
@@ -24,23 +32,36 @@ create_count_data_frames <-
     for (i in projects_vector) {
       success <- NULL
       attempt <- 1
+      # while loop to make three attempts to pull the recount3 data
       while( is.null(success) && attempt <= 3 ) {
         try({
           print(paste(i, 'attempt number:', attempt))
+          
           attempt <- attempt + 1
-          rse_gene = create_rse_manual(i, annotation = "gencode_v29")
+          if (GTEX){
+            rse_gene = create_rse(subset(gtex_data, project == i), annotation = "gencode_v29")
+          } else {
+            rse_gene = create_rse_manual(i, annotation = "gencode_v29")
+          }
           assays(rse_gene)$counts <- transform_counts(rse_gene)
-          
-          rse_gene_counts <- recount::getTPM(rse_gene, 
-                                             length_var = 'bp_length',
-                                             mapped_var = 'recount_qc.star.all_mapped_reads') %>% 
+          assays(rse_gene)$raw_counts <- compute_read_counts(rse_gene)
+          # extract TPM
+          rse_gene_TPM <- recount::getTPM(rse_gene, 
+                                          length_var = 'bp_length',
+                                          mapped_var = 'recount_qc.star.all_mapped_reads') %>% 
             data.frame()
-          rse_gene_counts <- rownames_to_column(rse_gene_counts, var = "gene_id")
+          rse_gene_TPM <- rownames_to_column(rse_gene_TPM, var = "gene_id")
           
+          row.names(rse_gene_TPM) <- rse_gene_TPM$gene_id
+          rse_gene_TPM <- rse_gene_TPM[,-1, drop=FALSE]
+          TPM_data_list[[i]] <- rse_gene_TPM
+          # extract raw counts
+          rse_gene_counts <- assays(rse_gene)$raw_counts %>% data.frame()
+          rse_gene_counts <- rownames_to_column(rse_gene_counts, var = "gene_id")
           row.names(rse_gene_counts) <- rse_gene_counts$gene_id
           rse_gene_counts <- rse_gene_counts[,-1, drop=FALSE]
           counts_data_list[[i]] <- rse_gene_counts
-          #Input mapping data
+          #extract mapping data
           mapping_data <- colData(rse_gene) %>% as.data.frame()
           mapping_data <- mapping_data %>% mutate(across(everything(), as.character))
           mapping_data_list[[i]] <- mapping_data
@@ -49,51 +70,78 @@ create_count_data_frames <-
         })
       }
     }
-    
+    ############################################
     # confirm gene name equality across each data frame
     gene_name_list <- list()
-    for (i in 1:length(counts_data_list)){
-      gene_name_list[[i]] <- row.names(counts_data_list[[i]])
+    for (i in 1:length(TPM_data_list)){
+      gene_name_list[[i]] <- row.names(TPM_data_list[[i]])
     }
     gene_name_matrix <- gene_name_list %>% do.call(cbind, .)
     ## counting equality across each gene name row
     if (sum(!apply(gene_name_matrix, 1, function(x) all(x==x[1]))) > 0){
       stop("Gene names not aligned")
     }
-    # cbind to make large matrix
-    counts_data_frame <- do.call(cbind, counts_data_list)
-    # remove name of study from column name
-    colnames(counts_data_frame) <- c(str_extract(colnames(counts_data_frame), '\\SRR\\S+'))
-    #Add rownames back
-    counts_data_frame <- counts_data_frame %>% as_tibble(rownames = 'gene_id')
+    ##########################################
     
+    ##########################################
+    # cbind to make large matrix
+    TPM_data_frame <- do.call(cbind, TPM_data_list)
+    count_data_frame <- do.call(cbind, counts_data_list)
+    # fix column names
+    if (GTEX){
+      #Using if-else to account for differences in the naming of the bone marrow samples included
+      colnames(TPM_data_frame) <- 
+        ifelse(!grepl("GTEX", colnames(TPM_data_frame)), c(str_split(colnames(TPM_data_frame), '.+?(?=K.562)') %>% map(2) %>% unlist()),
+               c(str_split(colnames(TPM_data_frame), '.+?(?=GTEX)') %>% map(2) %>% unlist()))
+      colnames(count_data_frame) <- 
+        ifelse(!grepl("GTEX", colnames(count_data_frame)), c(str_split(colnames(count_data_frame), '.+?(?=K.562)') %>% map(2) %>% unlist()),
+               c(str_split(colnames(count_data_frame), '.+?(?=GTEX)') %>% map(2) %>% unlist()))
+    } else {
+      # remove name of study from column name
+      colnames(TPM_data_frame) <- colnames(TPM_data_frame) %>% gsub('.*\\.','',.)
+      colnames(count_data_frame) <- colnames(count_data_frame) %>% gsub('.*\\.','',.)
+    }
+    
+    #Add rownames back
+    TPM_data_frame <- TPM_data_frame %>% as_tibble(rownames = 'gene_id')
+    count_data_frame <- count_data_frame %>% as_tibble(rownames = 'gene_id')
+    #########################################
+    
+
+    
+    #########################################
+    #filter matrix to only samples in our metadata
+    #Checking to make sure only data in the metadata is included in analysis
+    ## edit metadata run_accession to prepend an "X" if it starts with a digit
+    metadata$run_accession[grep("^\\d", metadata$run_accession)] <- paste0("X", metadata$run_accession[grep("^\\d", metadata$run_accession)])
+    TPM_data_frame_keep <- intersect(names(TPM_data_frame), metadata$run_accession)
+    TPM_data_frame_final <- TPM_data_frame %>% select(one_of(c("gene_id", TPM_data_frame_keep)))
+    
+    count_data_frame_keep <- intersect(names(count_data_frame), metadata$run_accession)
+    count_data_frame_final <- count_data_frame %>% select(one_of(c("gene_id", count_data_frame_keep)))
     #Create counts file
     system('mkdir -p gene_counts')
-    write.csv(counts_data_frame, 
-              file = paste0("gene_counts/", count_file_name, ".csv"), row.names = FALSE)
+    write_csv(TPM_data_frame_final, 
+              file = paste0("gene_counts/", TPM_matrix_file, ".csv.gz"))
+    write_csv(count_data_frame_final, 
+              file = paste0("gene_counts/", count_matrix_file, ".csv.gz"))
     #Create mapping file
     system('mkdir -p mapping_data')
     mapping_data_frame <- bind_rows(mapping_data_list)
-    write.csv(mapping_data_frame, 
-              file = paste0("mapping_data/", mapping_file_name, ".csv"), row.names = FALSE)
+    write_csv(mapping_data_frame, 
+              file = paste0("mapping_data/", mapping_file_name, ".csv.gz"))
     
-    #Aggregating the data
-    #Checking to make sure only data in the metadata is included in analysis
-    counts_data_frame_keep <- intersect(names(counts_data_frame), metadata$run_accession)
-    counts_data_frame_final <- counts_data_frame %>% select(one_of(c("gene_id", counts_data_frame_keep)))
-    
-    #Creating empty list to store aggregated counts in
-    aggregated_counts_data_list <- list()
+    ######################################
+    # Make long
     #Obtain study names so data can be subset during aggregation
-    aggregation_subset_data <- 
-      counts_data_frame_final %>% select(-"gene_id") %>% colnames() %>% as_tibble() %>% 
-      rename("value" = "run_accession") %>% 
-      left_join(metadata %>% select("study_accession", "run_accession"), by= "run_accession")
+    studies <- metadata %>% filter(run_accession %in% colnames(TPM_data_frame_final)) %>% pull(study_accession) %>% unique()
     
-    for (i in aggregation_subset_data$study_accession %>% unique()){
+    # make TPM long
+    long_TPM_data_list <- list()
+    for (i in studies){
       #Subset data to ensure memory is not exhausted
-      run_accessions = metadata %>% filter(study_accession == i) %>% pull(run_accession)
-      counts_subset <- counts_data_frame_final[, c("gene_id", run_accessions)]
+      run_accessions = metadata %>% filter(study_accession == i, run_accession %in% colnames(TPM_data_frame_final)) %>% pull(run_accession)
+      counts_subset <- TPM_data_frame_final[, c("gene_id", run_accessions)]
       #Reformatting data for aggregation
       long_counts_subset <- counts_subset %>% pivot_longer(-gene_id)
       names(long_counts_subset) <- c("gene_id", "run_accession", "value")
@@ -103,118 +151,41 @@ create_count_data_frames <-
       dt_long_counts <- data.table(long_counts_meta)
       long_counts_srs <- dt_long_counts[, .(value=mean(value)), by=list(gene_id, sample_accession)]
       
-      aggregated_counts_data_list[[i]] <- long_counts_srs
+      long_TPM_data_list[[i]] <- long_counts_srs
     }
+    
     #Bind rows to create aggregated count data frame
-    aggregated_counts_data <- aggregated_counts_data_list %>% bind_rows() %>% unique()
+    long_TPM <- long_TPM_data_list %>% bind_rows() %>% unique()
     #Create aggregated counts file
-    write.csv(aggregated_counts_data,
-              file = paste0("gene_counts/", aggregated_count_file_name, ".csv"), row.names = FALSE)
-  }
-
-
-create_gtex_count_data_frames <- function(projects_vector, 
-                                          count_file_name, 
-                                          aggregated_count_file_name, 
-                                          mapping_file_name, 
-                                          metadata,
-                                          empty_cache = TRUE) {
-  ### Function to create count data for GTEX samples -----
-  # Since the GTEX data in recount3 is labeled differently, we will use a different function to locate and download this data
-  
-  
-  if (empty_cache){
-    print("Clearing recount3 cache")
-    recount3_cache_rm()
-  }
-  #Indicating which recount library the function will pull from
-  options(recount3_url = "http://duffel.rail.bio/recount3/")
-  human_projects <- available_projects()
-  #Subset to only include GTEX data
-  gtex_data <- subset(human_projects, file_source == "gtex" & project_type == "data_sources")
-  #Create an empty list to gather the count data
-  counts_data_list <- list()
-  #Create an empty list to gather mapping information
-  mapping_data_list <- list()
-  
-  # creates list of count data frames
-  for (i in projects_vector) {
-    print(i)  
-    rse_gene = create_rse(subset(gtex_data, project == i), annotation = "gencode_v29")
-    # cut down to only in metadata
-    rse_gene <- rse_gene[,gsub('-','.', colnames(rse_gene)) %in% metadata$run_accession]
-    assays(rse_gene)$counts <- transform_counts(rse_gene)
+    write_csv(long_TPM,
+              file = paste0("gene_counts/", long_TPM_matrix_file, ".csv.gz"))
+    #############################################################################
     
-    rse_gene_counts <- recount::getTPM(rse_gene, 
-                                       length_var = 'bp_length',
-                                       mapped_var = 'recount_qc.star.all_mapped_reads') %>% 
-      data.frame()
-    rse_gene_counts <- rownames_to_column(rse_gene_counts, var = "gene_id")
     
-    row.names(rse_gene_counts) <- rse_gene_counts[,1]
-    rse_gene_counts <- rse_gene_counts[,-1]
-  
-    counts_data_list[[i]] <- rse_gene_counts
-    #Input mapping data
-    mapping_data <- colData(rse_gene) %>% as.data.frame()
-    mapping_data <- mapping_data %>% mutate(across(everything(), as.character))
-    mapping_data_list[[i]] <- mapping_data
+    ########################################################################################
+    # make count long
+    long_count_data_list <- list()
+    for (i in studies){
+      #Subset data to ensure memory is not exhausted
+      run_accessions = metadata %>% filter(study_accession == i, run_accession %in% colnames(count_data_frame_final)) %>% pull(run_accession)
+      counts_subset <- count_data_frame_final[, c("gene_id", run_accessions)]
+      #Reformatting data for aggregation
+      long_counts_subset <- counts_subset %>% pivot_longer(-gene_id)
+      names(long_counts_subset) <- c("gene_id", "run_accession", "value")
+      #Joining metadata for aggregation
+      long_counts_meta <- long_counts_subset %>% left_join(metadata %>% as_tibble() %>% select(sample_accession, run_accession) %>% unique(),
+                                                           by = c('run_accession'))
+      dt_long_counts <- data.table(long_counts_meta)
+      long_counts_srs <- dt_long_counts[, .(value=mean(value)), by=list(gene_id, sample_accession)]
+      
+      long_count_data_list[[i]] <- long_counts_srs
+    }
+    
+    
+    #Bind rows to create aggregated count data frame
+    long_count <- long_count_data_list %>% bind_rows() %>% unique()
+    #Create aggregated counts file
+    write_csv(long_count,
+              file = paste0("gene_counts/", long_count_matrix_file, ".csv.gz"))
+    ######################################################################################
   }
-  
-  # confirm gene name equality across each data frame
-  gene_name_list <- list()
-  for (i in 1:length(counts_data_list)){
-    gene_name_list[[i]] <- row.names(counts_data_list[[i]])
-  }
-  gene_name_matrix <- gene_name_list %>% do.call(cbind, .)
-  ## counting equality across each gene name row
-  if (sum(!apply(gene_name_matrix, 1, function(x) all(x==x[1]))) > 0){
-    stop("Gene names not aligned")
-  }
-  
-  # cbind to make large matrix
-  counts_data_frame <- do.call(cbind, counts_data_list)
-  # remove name of study from column name
-  #Using if-else to account for differences in the naming of the bone marrow samples included
-  colnames(counts_data_frame) <- 
-    ifelse(!grepl("GTEX", colnames(counts_data_frame)), c(str_split(colnames(counts_data_frame), '.+?(?=K.562)') %>% map(2) %>% unlist()),
-           c(str_split(colnames(counts_data_frame), '.+?(?=GTEX)') %>% map(2) %>% unlist()))
-  
-  #Add rownames back
-  counts_data_frame <- counts_data_frame %>% as_tibble(rownames = 'gene_id')
-  
-  #Create counts file
-  system('mkdir -p gene_counts')
-  write.csv(counts_data_frame, 
-            file = paste0("gene_counts/", count_file_name, ".csv"), row.names = FALSE)
-  
-  #Create mapping file
-  system('mkdir -p mapping_data')
-  mapping_data_frame <- bind_rows(mapping_data_list)
-  write.csv(mapping_data_frame, 
-            file = paste0("mapping_data/", mapping_file_name, ".csv"), row.names = FALSE)
-  
-  #Aggregating the data
-  #Checking to make sure only data in the metadata is included in analysis
-  counts_data_frame_keep <- intersect(names(counts_data_frame), metadata$run_accession)
-  counts_data_frame_final <- counts_data_frame %>% select(one_of(c("gene_id", counts_data_frame_keep)))
-  
-  #Creating empty list to store aggregated counts in
-  aggregated_counts_data_list <- list()
-  #Obtain study names so data can be subset during aggregation
-  long_counts <- counts_data_frame_final %>% pivot_longer(-gene_id)
-  names(long_counts) <- c("gene_id", "run_accession", "value")
-  #Joining metadata for aggregation
-  long_counts_meta <- long_counts %>% left_join(metadata %>% as_tibble() %>% select(sample_accession, run_accession) %>% unique(),
-                                                by = c('run_accession'))
-  dt_long_counts <- data.table(long_counts_meta)
-  long_counts_srs <- dt_long_counts[, value:=mean(value), by=c("sample_accession", "gene_id")] %>% as_tibble() %>% select(-run_accession)
-  
-  #aggregated_counts_data_list[[i]] <- long_counts_srs
-  
-  #Bind rows to create aggregated count data frame
-  #aggregated_counts_data <- aggregated_counts_data_list %>% bind_rows()
-  #Create aggregated counts file
-  write.csv(long_counts_srs,
-            file = paste0("gene_counts/", aggregated_count_file_name, ".csv"), row.names = FALSE)
-}
--- scripts/unifier_to_counts.R
+++ scripts/unifier_to_counts.R
@@ -18,54 +18,80 @@ create_count_data_frames("http://duffel.rail.bio/recount3/",
                            "SRP098761", "SRP105756", "SRP106457", "SRP108292", "SRP110135", "SRP111145",
                            "SRP115908", "SRP117613", "SRP119291", "SRP119766", "SRP151763", "SRP159246",
                            "ERP022243", "SRP186009", "SRP156453", "SRP136195"),
-                         "recount3_transformed_counts",
-                         "aggregated_recount3_transformed_counts",
+                         "recount3_TPM",
+                         "long_recount3_TPM",
+                         "recount3_count",
+                         "long_recount3_count",
                          "recount3_mapping_information",
-                         emeta, empty_cache = FALSE)
+                         emeta, 
+                         empty_cache = FALSE,
+                         GTEX = FALSE)
 
 
-create_gtex_count_data_frames(
+create_count_data_frames("http://duffel.rail.bio/recount3/",
                               c("ADIPOSE_TISSUE", "MUSCLE", "BLOOD_VESSEL", "HEART", "OVARY", "UTERUS",
                                 "VAGINA", "BREAST", "SKIN", "SALIVARY_GLAND", "BRAIN", "ADRENAL_GLAND",
                                 "THYROID", "LUNG", "SPLEEN", "PANCREAS", "ESOPHAGUS", "STOMACH", "COLON",
                                 "SMALL_INTESTINE", "PROSTATE", "TESTIS", "NERVE", "PITUITARY", "BLOOD",
                                 "LIVER", "KIDNEY",   "CERVIX_UTERI", "FALLOPIAN_TUBE", "BLADDER", "BONE_MARROW"),
-                              "gtex_transformed_counts",
-                              "aggregated_gtex_transformed_counts",
+                              "GTEX_TPM",
+                              "long_GTEX_TPM",
+                              "GTEX_count",
+                              "long_GTEX_count",
                               "gtex_mapping_information",
-                              emeta, empty_cache = FALSE)
+                              emeta, 
+                              empty_cache = FALSE,
+                              GTEX = TRUE)
 
 
 
 create_count_data_frames("~/data/eiad_rse/rse/",
                          emeta %>% filter(data_location == 'local') %>% pull(study_accession) %>% unique(),
-                         "local_transformed_counts",
-                         "aggregated_local_transformed_counts",
+                         "local_TPM",
+                         "long_local_TPM",
+                         "local_count",
+                         "long_local_count",
                          "local_mapping_information",
-                         emeta, empty_cache = FALSE)
+                         emeta, 
+                         empty_cache = FALSE, 
+                         GTEX = FALSE)
 
 
 
-# Calling aggregated data
-aggregated_recount3_transformed_counts <- vroom::vroom("gene_counts/aggregated_recount3_transformed_counts.csv")
-aggregated_gtex_transformed_counts <- vroom::vroom("gene_counts/aggregated_gtex_transformed_counts.csv")
-aggregated_local_data_additions_transformed_counts <- vroom::vroom("gene_counts/aggregated_local_transformed_counts.csv")
+
+# input long TPM
+recount3_TPM <- vroom::vroom("gene_counts/long_recount3_TPM.csv.gz") 
+gtex_TPM <- vroom::vroom("gene_counts/long_GTEX_TPM.csv.gz")
+local_TPM <- vroom::vroom("gene_counts/long_local_TPM.csv.gz") 
+
+# input count matrices
+recount3_counts <- vroom::vroom("gene_counts/recount3_count.csv.gz") %>% data.frame()
+gtex_counts <- vroom::vroom("gene_counts/GTEX_count.csv.gz") %>% data.frame()
+local_counts <- vroom::vroom("gene_counts/local_count.csv.gz")  %>% data.frame()
 
 # Calling aggregated metadata
-aggregated_recount3_metadata <- vroom::vroom("mapping_data/recount3_mapping_information.csv")
-aggregated_gtex_transformed_counts <- vroom::vroom("mapping_data/gtex_mapping_information.csv")
-aggregated_local_data_additions_transformed_counts <- vroom::vroom("mapping_data/local_mapping_information.csv")
-r3metadata <- bind_rows(aggregated_recount3_metadata, aggregated_gtex_transformed_counts, aggregated_local_data_additions_transformed_counts )
+recount3_metadata <- vroom::vroom("mapping_data/recount3_mapping_information.csv.gz")
+gtex_metadata <- vroom::vroom("mapping_data/gtex_mapping_information.csv.gz")
+local_metadata <- vroom::vroom("mapping_data/local_mapping_information.csv.gz")
+r3metadata <- bind_rows(recount3_metadata, gtex_metadata, local_metadata )
 
-# Combine aggregated data
-gene_counts <- bind_rows(aggregated_recount3_transformed_counts,
-                         aggregated_gtex_transformed_counts,
-                         aggregated_local_data_additions_transformed_counts) %>% 
+# Combine long TPM
+gene_TPM <- bind_rows(recount3_TPM,
+                      gtex_TPM,
+                      local_TPM) %>% 
   unique()
 
-mat <- gene_counts %>% unique() %>% pivot_wider(values_from = value, names_from = sample_accession)
+# make matrix TPM
+mat_TPM <- gene_TPM %>% unique() %>% pivot_wider(values_from = value, names_from = sample_accession)
+
+# make matrix counts
+mat <- cbind((recount3_counts %>% data.frame())[,2:ncol(recount3_counts)],
+      (local_counts %>% data.frame())[,2:ncol(local_counts)],
+      (gtex_counts %>% data.frame())[,2:ncol(gtex_counts)])
+row.names(mat) <- recount3_counts[,1]
 
 ### Write files ---------
-write_csv(gene_counts, "gene_counts/gene_counts.csv.gz", progress = TRUE)
+write_csv(gene_TPM, "gene_counts/gene_TPM.csv.gz", progress = TRUE)
+write_csv(mat_TPM, "gene_counts/gene_TPM_matrix.csv.gz", progress = TRUE)
 write_csv(mat, "gene_counts/gene_counts_matrix.csv.gz", progress = TRUE)
 write_csv(r3metadata, "mapping_data/recount_metadata.csv.gz")
