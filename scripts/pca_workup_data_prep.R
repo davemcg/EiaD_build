ab5b246a4914155b3b542039f160f2dc80f92f41
David McGaughey <mcgaughey@gmail.com>
2023-03-06 12:50:34 -0500

    pca analysis with counts instead of tpm

--- scripts/pca_workup_data_prep.R
+++ scripts/pca_workup_data_prep.R
@@ -3,7 +3,7 @@ library(tidyverse)
 
 ##################
 # Load in metadata
-emeta <- data.table::fread('data/eyeIntegration22_meta_2022_11_16.01.csv') %>% as_tibble()
+emeta <- data.table::fread('data/eyeIntegration22_meta_2023_03_03.csv.gz') %>% as_tibble()
 ##################
 
 ###################
@@ -16,8 +16,8 @@ mat <- vroom::vroom("gene_counts/gene_counts_matrix.csv.gz") %>% data.frame()
 
 ######################
 # Light processing
-# set rownames
-row.names(mat) <- mat[,1]
+# set rownames and remove .digit at the end
+row.names(mat) <- mat[,1] %>% gsub('\\.\\d+','',.)
 # remove gene column
 mat <- mat[,2:ncol(mat)]
 ##########################
@@ -37,49 +37,33 @@ gene_count_meta <- colnames(mat) %>%
 library(DESeq2)
 dds <- DESeqDataSetFromMatrix(mat, gene_count_meta, design = ~ Tissue)
 dds <- collapseReplicates(dds, groupby = colData(dds)$sample_accession)
-#############################################
 
-#########################
-# calculate TPM
-# pull counts out
-raw_counts <- assay(dds, 'counts')
-# get length info ('score' from recount3 gtf)
-# http://duffel.rail.bio/recount3/human/new_annotations/gene_sums/human.gene_sums.G029.gtf.gz
-gene_annotations <- 
-  rtracklayer::import("data/human.gene_sums.G029.gtf.gz") %>% 
-  as.data.frame()
-len_info <- row.names(raw_counts) %>% enframe() %>% mutate(geneid = str_extract(value, 'ENSG\\d+') %>% gsub(')','',.)) %>% 
-  left_join(gene_annotations %>% mutate(geneid = gsub('\\.\\d+','',gene_id))) 
-# Divide each gene by transcript length
-TPMlen <- apply( raw_counts, 2, function(x){ x / len_info$score } )
-# Divide again the transcript length and apply 1e6 multiplier
-TPM <- apply( TPMlen, 2, function(x) { x / sum(x) * 1E6} )
-#########################################
-
-
+# now extract the sample accession level counts
+sample_mat <- assay(dds, 'counts')
 # merge with TPMpb (pb is pseudoBulk)
-## first run a diff script to get scEiaD/plae data
-source('scripts/scEiaD_PBcounts_toTPM.R')
-# remove .1, etc at the ends of ensgene
-row.names(TPM) <- row.names(TPM) %>% gsub("\\.\\d+$","",.)
-TPMall <- cbind(TPM[intersect(row.names(TPM) , row.names(TPMpb)),],
-                TPMpb[intersect(row.names(TPM) , row.names(TPMpb)),])
+
+# load in scRNA pseudobulk counts data from plae/scEiaD
+pb <- read_tsv('http://hpc.nih.gov/~mcgaugheyd/scEiaD/2022_03_22/4000-counts-universe-study_accession-scANVIprojection-15-5-20-50-0.1-CellType_predict-Homo_sapiens.Staged.pseudoCounts.tsv.gz') 
+pb_genes <- pb$Gene
+pb_mat <- pb[,-1] %>% as.matrix()
+row.names(pb_mat) <- pb_genes
+
+mat_all <- cbind(sample_mat[intersect(row.names(sample_mat) , row.names(pb_mat)),],
+                pb_mat[intersect(row.names(sample_mat) , row.names(pb_mat)),])
 
 
 
 #####################################
 # add in human readable gene names
-row.names(TPMall) <- row.names(TPMall) %>% 
+gene_annotations <-
+  rtracklayer::import("data/human.gene_sums.G029.gtf.gz") %>%
+  as.data.frame()
+row.names(mat_all) <- row.names(mat_all) %>% 
   enframe(value = 'gene_id') %>% 
   left_join(gene_annotations %>% mutate(gene_id = gsub("\\.\\d+$","",gene_id))) %>% 
   mutate(ID = paste0(gene_name, ' (', gene_id, ')')) %>% 
   pull(ID)
 
-row.names(TPM) <- row.names(TPM) %>% 
-  enframe(value = 'gene_id') %>% 
-  left_join(gene_annotations %>% mutate(gene_id = gsub("\\.\\d+$","",gene_id))) %>% 
-  mutate(ID = paste0(gene_name, ' (', gene_id, ')')) %>% 
-  pull(ID)
 ########################################################
 
 ######################################################
@@ -87,20 +71,18 @@ row.names(TPM) <- row.names(TPM) %>%
 ## cant use previous gene_count_meta as that was built around run_accession
 ## now we have sample level counts
 ## also sc data
-meta_TPMall <- colnames(TPMall) %>% 
+meta_mat_all <- colnames(mat_all) %>% 
   enframe(value = 'sample_accession') %>% 
   left_join(gene_count_meta %>% dplyr::select(-run_accession, -name)%>% dplyr::distinct(), by = 'sample_accession') %>% 
   # now label the scRNA pb cell types
-  mutate(CellType = gsub("^.*__","",sample_accession)) %>% 
-  left_join(sc_tissue_ct %>% dplyr::rename(scTissue = Tissue), by = 'CellType') %>% 
-  mutate(Tissue = case_when(is.na(Tissue) ~ scTissue,
-                            TRUE ~ Tissue),
-         Cohort = case_when(is.na(Cohort) ~ Organ,
-                            TRUE ~ Cohort),
-         Source = case_when(!is.na(scTissue) ~ 'scRNA',
+  mutate(CellType = case_when(grepl('__', sample_accession) ~ str_extract(sample_accession, '__.*__') %>% gsub('__','',.)),
+         Age = case_when(grepl('Matur', sample_accession) ~ 'Adult',
+                         grepl("Dev\\.$", sample_accession) ~ 'Fetal',
+                         TRUE ~ Age),
+         Source = case_when(grepl('__', sample_accession) ~ 'scRNA',
                             TRUE ~ Source))
 #########################################
-save(TPM, TPMpb, TPMall, emeta, meta_TPMall, sc_tissue_ct, file = 'data/EiaD_pca_analysis.Rdata')
-write_csv(TPMall %>% as_tibble(rownames = 'Gene'), file = 'data/EiaD_pca_analysis_TPMall.csv.gz')
-write_csv(meta_TPMall, file = 'data/EiaD_pca_analysis_meta_TPMall.csv.gz')
+save(meta_mat_all, mat_all, gene_annotations, file = 'data/EiaD_pca_analysis.Rdata')
+#write_csv(mat_all %>% as_tibble(rownames = 'Gene'), file = 'data/EiaD_pca_analysis_mat_all.csv.gz')
+#write_csv(meta_mat_all, file = 'data/EiaD_pca_analysis_meta_mat_all.csv.gz')
 #########################################
